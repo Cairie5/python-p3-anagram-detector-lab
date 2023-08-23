473cfc70a3f24a29ba0f57d273b761812098725f
Patience Wangari Muraguri <patiencewangari5@gmail.com>
2023-08-23 18:08:25 +0300

    Completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+        
+    def match(self, words):
+        return [w for w in words if self.is_anagram(w)]
+    
+    def is_anagram(self, candidate):
+        candidate_lower = candidate.lower()
+        return candidate_lower != self.word and sorted(candidate_lower) == sorted(self.word)
+    
